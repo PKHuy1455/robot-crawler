8cb4c69a47da83550efe3b3175b64091542f1352
PKHuy1455 <huyphan1455@gmail.com>
2026-05-16 09:15:19 +0700

    feat: firmware v3 deadband + PID tuning, hardware_driver node

--- src/robot_hardware/setup.py
+++ src/robot_hardware/setup.py
@@ -4,26 +4,25 @@ package_name = 'robot_hardware'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='1.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/hardware_driver.launch.py']),
+        ('share/' + package_name + '/config', ['config/hardware_params.yaml']),
     ],
-    install_requires=['setuptools'],
+    install_requires=['setuptools', 'pyserial'],
     zip_safe=True,
-    maintainer='pi',
-    maintainer_email='pi@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
+    maintainer='user',
+    maintainer_email='user@todo.com',
+    description='Hardware driver node for differential drive tracked robot.',
+    license='MIT',
+    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'hardware_driver = robot_hardware.hardware_driver:main',
         ],
     },
 )
